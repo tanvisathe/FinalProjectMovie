ddd4a66d4ad40f7557ddf0a28d82c168f9943dfa
Tanvi <tanvi1sathe@gmail.com>
2017-11-15 23:01:03 -0500

    Text.txt

--- MovieQuizApp/App_Start/BundleConfig.cs
+++ MovieQuizApp/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace MovieQuizApp
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-darkly.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
